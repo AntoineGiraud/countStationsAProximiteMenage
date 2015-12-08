1a4a66d901c2dd1f217b6c1d07b7382514afc353
AntoineGiraud <antoine_giraud2@hotmail.fr>
2015-12-07 21:52:40 -0500

    script pour compter les stations autour des ménages de l'enquête OD

--- findStopsAroundHomesOfODsurvey.py
+++ findStopsAroundHomesOfODsurvey.py
@@ -1,6 +1,7 @@
 import lib.toolbox as toolbox
 import time
 import csv
+import math
 import os
 import json
 from collections import OrderedDict
@@ -60,47 +61,45 @@ def loadFile(fileName, nbLignes, idPrincipale, mapping):
     return stops, labels
 
 
-def countStopTimesPerStop(fileName, nbLignes, idPrincipale, mapping, stops):
+def getDistance(ptA, ptB):
+    return math.sqrt(math.pow(ptB[0]-ptA[0], 2) + math.pow(ptB[1]-ptA[1], 2))
+
+
+def countStopTimesPerStop(menages, menages_label, nbLignes, stopsToCheck, name, newCounter):
     tStart = time.clock()
-    labels = []
+    menages_label.append('1kmCount'+name+'stops')
+    menages_label.append('500mCount'+name+'stops')
+    for c in newCounter:
+        menages_label.append('1kmCount'+name+c)
+        menages_label.append('500mCount'+name+c)
 
-    stopIdPos = 3
-    tripIdPos = 0
-    isGtfsStm = 'stm' in fileName or 'STM' in fileName or 'Stm' in fileName
-
-    i = -1
-    for line in open(fileName, encoding="utf8"):  # , encoding="utf8"
-        ligne = line.replace("\n", "").split(",")
-        # name;lon;lat;coordsLatLon;capacity;region
-        if i < 0:
-            labels = ligne
-            stopIdPos = labels.index(idPrincipale)
-            tripIdPos = labels.index('trip_id')
-            i += 1
-            continue
-        elif i >= 20000000:
-            break  # On veut que 500 tapIn mais ... on ne veut pas couper les tap in d'une carte !
-
-        try:
-            stops[ligne[stopIdPos]]['countStopTimes'] += 1
-            if isGtfsStm:
-                tripId = ligne[tripIdPos]
-                if '_S_' in tripId:
-                    stops[ligne[stopIdPos]]['countStopTimesSemaine'] += 1
-                elif '_A_' in tripId:
-                    stops[ligne[stopIdPos]]['countStopTimesSamedi'] += 1
-                elif '_I_' in tripId:
-                    stops[ligne[stopIdPos]]['countStopTimesDimanche'] += 1
-        except Exception:
-            print(ligne[stopIdPos], 'inconu dans les stops du GTFS O.o,', stopIdPos)
+    i = 0
+    for menage in menages.values():
+        menage['1kmCount'+name+'stops'] = 0
+        menage['500mCount'+name+'stops'] = 0
+        for c in newCounter:
+            menage['1kmCount'+name+c] = 0
+            menage['500mCount'+name+c] = 0
+        ptA = [menage['mtmx'], menage['mtmy']]
+        for stop in stopsToCheck.values():
+            ptB = [stop['mtmx'], stop['mtmy']]
+            distance = getDistance(ptA, ptB)
+            if distance < 500:
+                menage['500mCount'+name+'stops'] += 1
+                for c in newCounter:
+                    menage['500mCount'+name+c] += stop[c]
+            if distance < 1000:
+                menage['1kmCount'+name+'stops'] += 1
+                for c in newCounter:
+                    menage['1kmCount'+name+c] += stop[c]
 
         toolbox.progressBar(i, nbLignes)
         i += 1
 
     toolbox.hideProgressBar()
-    print(i, "stopTimes lus en", toolbox.tempsCalulString(tStart), "et affectés aux stops pour", fileName)
+    print(len(stopsToCheck), 'stops affectés aux', i, "ménages en", toolbox.tempsCalulString(tStart), "pour les stops", name)
 
-print("----- Lecture des fichiers stops & stops_time AMT ------")
+print("----- Lecture des fichiers stops consolidés & ménages ------")
 mapping = {
     'mtmx': 'float',
     'mtmy': 'float',
@@ -115,18 +114,20 @@ mapping = {
     'countStopTimesDimanche': 'int',
     'capacity': 'int'
 }
-stopsAMT = loadFile('input/stationsAmtSous1kmMenagesOD08_mtm8_wgs64_avecSRIDU.csv', 123, 'stop_id', mapping)
-stopsBixi = loadFile('input/stationsBixi_mtm8_wgs64_avecSRIDU.csv', 463, 'stop_id', mapping)
-stopsSTM = loadFile('input/stationsStmSous1kmMenagesOD08_mtm8_wgs64_avecSRIDU.csv', 8995, 'stop_id', mapping)
+stopsAMT, stopsAMT_label = loadFile('input/stationsAmtSous1kmMenagesOD08_mtm8_wgs64_avecSRIDU.csv', 123, 'stop_id', mapping)
+stopsBixi, stopsBixi_label = loadFile('input/stationsBixi_mtm8_wgs64_avecSRIDU.csv', 463, 'stop_id', mapping)
+stopsSTM, stopsSTM_label = loadFile('input/stationsStmSous1kmMenagesOD08_mtm8_wgs64_avecSRIDU.csv', 8995, 'stop_id', mapping)
 
-menages = loadFile('input/menages_OD08_mtm8_pop_wgs64_mtm8.csv', 17749, 'idFeuillet', mapping)
+menages, menages_label = loadFile('input/menages_OD08_mtm8_pop_wgs64_mtm8.csv', 17749, 'idFeuillet', mapping)
 
-# print("----- Lecture des fichiers stops & stops_time STM ------")
+print("----- Comptage stops autour des ménages ------")
 
-# countStopTimesPerStop('input/GTFS/stop_times_stm.txt', 5136728, {}, stopsSTM)
+countStopTimesPerStop(menages, menages_label, 17749, stopsBixi, 'BIXI', ['capacity'])
+countStopTimesPerStop(menages, menages_label, 17749, stopsAMT, 'AMT', ['countStopTimes'])
+countStopTimesPerStop(menages, menages_label, 17749, stopsSTM, 'STM', ['countStopTimes', 'countStopTimesSemaine', 'countStopTimesSamedi', 'countStopTimesDimanche'])
 
-# with open('output/stops_stm_withCountStopTimes.csv', 'w', newline='') as csvfile:
-#     outputFile = csv.writer(csvfile, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-#     outputFile.writerow(labelsAMT)
-#     for id, row in stopsSTM.items():
-#         outputFile.writerow(list(row.values()))
+with open('output/menages_withCountStopTimes.csv', 'w', newline='') as csvfile:
+    outputFile = csv.writer(csvfile, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    outputFile.writerow(menages_label)
+    for row in menages.values():
+        outputFile.writerow(list(row.values()))
